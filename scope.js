5e1cf99b2f518b0bd56ed5b606381a5849fb279a
sahidularif <sahidularif1@gmail.com>
2021-02-03 22:28:39 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+const bonus = 10;const double = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    if( result > 10){
+        let mood = "happy";
+        let mood = "angry";
+        let mood = "tensed";
+        console.log(mood);
+    }
+   
+    console.log(mood);
+    return result;
+}
+const output = sum(7, 3);
+console.log('outer output is ', output);
+// console.log(output);
